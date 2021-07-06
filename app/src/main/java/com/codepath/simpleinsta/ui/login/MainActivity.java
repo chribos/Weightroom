161da3f138a870979e9559ca5c813465465ce6d7
chribos <cdb89@cornell.edu>
2021-07-06 18:05:16 -0400

    logout button

--- app/src/main/java/com/codepath/simpleinsta/ui/login/MainActivity.java
+++ app/src/main/java/com/codepath/simpleinsta/ui/login/MainActivity.java
@@ -1,16 +1,41 @@
 package com.codepath.simpleinsta.ui.login;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.codepath.simpleinsta.R;
+import com.parse.ParseUser;
 
 public class MainActivity  extends AppCompatActivity {
+    Button logout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logout = findViewById(R.id.logout);
+
+        //create click listener for logout button
+        logout.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick(View v) {
+                //log user out
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+                goLoginActivity();
+
+            }
+        });
+    }
+    private void goLoginActivity() {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
     }
 }
+
